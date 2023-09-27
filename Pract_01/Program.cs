92fb03ca0cf9a480e2eaa2315b88eae3d53053f7
Konstantin <lolkup705@gmail.com>
2023-09-27 19:56:43 +0900

    add Info Pract 01

---
+++ Pract_01/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.Write("Hello 1 ");
+
+/*
+    Console.Write("Hello 1 ")
+        Console - Обращение к консоли.
+        Write - Вывод текста в одну строку.
+        ("Hello 1 ") - Написание текста.
+*/
+
+Console.Write("Hello 2!\n\n ");
+
+/*
+    Console.Write("Hello 2!\n\n ")
+        Console - Обращение к консоли.
+        Write - Вывод текста в одну строку.
+        ("Hello 2!\n\n ") - Написание текста с переходом на новую строку
+        с командой \n\n.
+*/
+
+Console.WriteLine("Hello 3 ");
+
+/*
+    Console.WriteLine("Hello 3 ")
+        Console - Обращение к консоли.
+        WriteLine - Вывод текста с переходом на новую строку. 
+        ("Hello 3 ") - Написание текста.
+*/
+
+Console.ReadKey();
+
+/*
+    Console.ReadKey()
+        Console - Обращение к консоли.
+        ReadKey() - Программа не завершаеться пока не введут хоть один символ.
+*/
+
+
