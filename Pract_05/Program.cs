9809cf5f960057c7b6e15ed491992cde4d5849b0
Konstantin <lolkup705@gmail.com>
2023-12-12 09:29:44 +0900

    add Info

---
+++ Pract_05/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Оператор «Switch-case»!
+«Switch-case» - необходимоть проверить одну переменную на множество её возможных значений.
+*/
+
+short user_input = Convert.ToInt16(Console.ReadLine());
+
+switch(user_input)  {
+    case 5: 
+        Console.WriteLine("Number is 5 ");
+        break;
+    case 15: 
+        Console.WriteLine("Number is 15 "); 
+        break;
+    case 25: 
+        Console.WriteLine("Number is 25 "); 
+        break;
+    case 250: 
+        Console.WriteLine("Number is 250 "); 
+        break;
+    default:
+        Console.WriteLine("Number is unrecognized! ");
+        break;
+}
