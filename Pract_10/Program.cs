dafb3cd1cd7d055d6334e7beaf92774612050fbf
Konstantin <lolkup705@gmail.com>
2024-01-27 21:34:55 +0900

    add Info

---
+++ Pract_10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace project {
+
+    class Program {
+        static void Main(){
+           /* !!! Строка как массив данных !!!
+           
+           string word = "Hello";
+           word += "!!!";
+
+           // Console.WriteLine(word[1]);   - обращение к любому элемента массива.
+           // Console.WriteLine(word.Length);   - подсчёт кол-во элементов.
+
+             !!! Методы по работе со строками !!!
+
+            string word = "Hello";
+            word += "!";
+
+            word = String.Concat(word, "!!");    - функция "String.Concat()" позволяет взять определённую строку и добавить подстроку.
+            Console.WriteLine(word);
+            
+            !!!
+
+            string word = "Hello";
+            word += "!";
+
+            Console.WriteLine(String.Compare(word, "Hello"));    - функция "String.Compare()" сравнение строк.
+            
+            !!!
+
+            string people = "Alex,Bob,John";
+            string[] names = people.Split(new char[]{','});      - функция "Split()" разбивает строку по опр-нному символу.
+                foreach(string el in names)
+                Console.WriteLine(el);
+            
+            !!!
+
+                string[] names = people.Split(new char[]{','});     
+                people = string.Join(" ", names);                   - функция "string.Join()" позволяет опр-нный массив будь то строки или числа, приоброзовать в строку который будет записан через опр-нный символ.
+                Console.WriteLine(people);
+            
+            !!!
+
+            string word = "Hello";
+            word += "!";
+            // Console.WriteLine(word.Trim());  - функция "Trim()" позволяет удалить лишние пробелы до и после строки.
+            // Console.WriteLine(word.Substring(0, word.Length - 1));   - функция "Substring()" позволяет обрезать строку сначало или её конца.
+            
+            */  //!!! Работа с файлами. Внесение данных !!!
+
+            
+
+        }
+
+    }
+
+}
