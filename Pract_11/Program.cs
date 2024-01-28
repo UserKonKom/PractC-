d1013a9d4c07c9248bd1b35fda8e20bf56810829
Konstantin <lolkup705@gmail.com>
2024-01-28 19:50:41 +0900

    add Info

---
+++ Pract_11/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Обработчик исключений. Оператор try-catch и блок finally!!!
+
+using System;
+
+namespace project {
+
+    class Program {
+        static void Main(){
+            /*
+            
+            try {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            } catch(FormatException) {
+                Console.WriteLine("Вы ввели не тот формат! ");
+            }
+
+    !!!
+
+            try {
+                int a = 5, b = 0, res;
+                res = a / b;
+            } catch(DivideByZeroException) {
+                Console.WriteLine("Деление на ноль! ");
+            }
+
+            */
+
+            try {
+                int a, b, res;
+                Console.Write("Введите первое число: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                res = a / b;
+                Console.WriteLine("Результат: " + res);
+            } catch(DivideByZeroException) {
+                Console.WriteLine("Деление на ноль! ");
+            } catch(FormatException) {
+                Console.WriteLine("Вы ввели не тот формат! ");
+            } finally {
+
+            }
+            
+        }
+
+    }
+
+}
